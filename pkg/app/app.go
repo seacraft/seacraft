e10387595e939c60ca45b9425cb6c2a18af06324
huhouhuam@outlook.com <huhouhuam@outlook.com>
2023-08-10 00:38:37 +0800

    inti

---
+++ pkg/app/app.go
@@ -0,0 +1,179 @@
+/*
+ *  Copyright 2023 The seacraft Authors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package app
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+// App is the main structure of a cli application.
+// It is recommended that an app be created with the app.NewApp() function.
+type App struct {
+	basename    string
+	name        string
+	description string
+	options     CliOptions
+	runFunc     RunFunc
+	silence     bool
+	noVersion   bool
+	noConfig    bool
+	commands    []*Command
+	args        cobra.PositionalArgs
+	cmd         *cobra.Command
+}
+
+// Option defines optional parameters for initializing the application
+// structure.
+type Option func(*App)
+
+// WithOptions to open the application's function to read from the command line
+// or read parameters from the configuration file.
+func WithOptions(opt CliOptions) Option {
+	return func(a *App) {
+		a.options = opt
+	}
+}
+
+// RunFunc defines the application's startup callback function.
+type RunFunc func(basename string) error
+
+// WithRunFunc is used to set the application startup callback function option.
+func WithRunFunc(run RunFunc) Option {
+	return func(a *App) {
+		a.runFunc = run
+	}
+}
+
+// WithDescription is used to set the description of the application.
+func WithDescription(desc string) Option {
+	return func(a *App) {
+		a.description = desc
+	}
+}
+
+// WithSilence sets the application to silent mode, in which the program startup
+// information, configuration information, and version information are not
+// printed in the console.
+func WithSilence() Option {
+	return func(a *App) {
+		a.silence = true
+	}
+}
+
+// WithNoVersion set the application does not provide version flag.
+func WithNoVersion() Option {
+	return func(a *App) {
+		a.noVersion = true
+	}
+}
+
+// WithNoConfig set the application does not provide config flag.
+func WithNoConfig() Option {
+	return func(a *App) {
+		a.noConfig = true
+	}
+}
+
+// WithValidArgs set the validation function to valid non-flag arguments.
+func WithValidArgs(args cobra.PositionalArgs) Option {
+	return func(a *App) {
+		a.args = args
+	}
+}
+
+// WithDefaultValidArgs set default validation function to valid non-flag arguments.
+func WithDefaultValidArgs() Option {
+	return func(a *App) {
+		a.args = func(cmd *cobra.Command, args []string) error {
+			for _, arg := range args {
+				if len(arg) > 0 {
+					return fmt.Errorf("%q does not take any arguments, got %q", cmd.CommandPath(), args)
+				}
+			}
+
+			return nil
+		}
+	}
+}
+
+// NewApp creates a new application instance based on the given application name,
+// binary name, and other options.
+func NewApp(name string, basename string, opts ...Option) *App {
+	a := &App{
+		name:     name,
+		basename: basename,
+	}
+
+	for _, o := range opts {
+		o(a)
+	}
+
+	a.buildCommand()
+
+	return a
+}
+
+func (a *App) buildCommand() {
+	cmd := cobra.Command{
+		Use:   FormatBaseName(a.basename),
+		Short: a.name,
+		Long:  a.description,
+		// stop printing usage when the command errors
+		SilenceUsage:  true,
+		SilenceErrors: true,
+		Args:          a.args,
+	}
+	// cmd.SetUsageTemplate(usageTemplate)
+	cmd.SetOut(os.Stdout)
+	cmd.SetErr(os.Stderr)
+	cmd.Flags().SortFlags = true
+	cliflag.InitFlags(cmd.Flags())
+
+	if len(a.commands) > 0 {
+		for _, command := range a.commands {
+			cmd.AddCommand(command.cobraCommand())
+		}
+		cmd.SetHelpCommand(helpCommand(FormatBaseName(a.basename)))
+	}
+	if a.runFunc != nil {
+		cmd.RunE = a.runCommand
+	}
+
+	var namedFlagSets cliflag.NamedFlagSets
+	if a.options != nil {
+		namedFlagSets = a.options.Flags()
+		fs := cmd.Flags()
+		for _, f := range namedFlagSets.FlagSets {
+			fs.AddFlagSet(f)
+		}
+	}
+
+	if !a.noVersion {
+		verflag.AddFlags(namedFlagSets.FlagSet("global"))
+	}
+	if !a.noConfig {
+		addConfigFlag(a.basename, namedFlagSets.FlagSet("global"))
+	}
+	globalflag.AddGlobalFlags(namedFlagSets.FlagSet("global"), cmd.Name())
+	// add new global flagset to cmd FlagSet
+	cmd.Flags().AddFlagSet(namedFlagSets.FlagSet("global"))
+
+	addCmdTemplate(&cmd, namedFlagSets)
+	a.cmd = &cmd
+}
---
+++ pkg/app/options.go
@@ -0,0 +1,44 @@
+/*
+ *  Copyright 2023 The seacraft Authors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package app
+
+// CliOptions abstracts configuration options for reading parameters from the
+// command line.
+type CliOptions interface {
+	// AddFlags adds flags to the specified FlagSet object.
+	// AddFlags(fs *pflag.FlagSet)
+	Flags() (fss cliflag.NamedFlagSets)
+	Validate() []error
+}
+
+// ConfigurableOptions abstracts configuration options for reading parameters
+// from a configuration file.
+type ConfigurableOptions interface {
+	// ApplyFlags parsing parameters from the command line or configuration file
+	// to the options instance.
+	ApplyFlags() []error
+}
+
+// CompleteableOptions abstracts options which can be completed.
+type CompleteableOptions interface {
+	Complete() error
+}
+
+// PrintableOptions abstracts options which can be printed.
+type PrintableOptions interface {
+	String() string
+}
